f02a32c2b8b85a20b0238be71837678dababf918
myunghakLee <mh9716@naver.com>
2021-04-24 14:24:20 +0000

    sh

---
+++ vgg.sh
@@ -0,0 +1,6 @@
+python train.py -net vgg16 -gpu --random_rate 0.1 --save_dir experiments/vgg16_10
+python train.py -net vgg16 -gpu --random_rate 0.2 --save_dir experiments/vgg16_20
+python train.py -net vgg16 -gpu --random_rate 0.3 --save_dir experiments/vgg16_30
+python train.py -net vgg16 -gpu --random_rate 0.4 --save_dir experiments/vgg16_40
+python train.py -net vgg16 -gpu --random_rate 0.5 --save_dir experiments/vgg16_50
+
